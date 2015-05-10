51d055526741a70e7b36512bbf8c7a605dfdcbfc
Torbjorn Skyberg Knutsen <torbjornk@mesan.no>
2015-05-10 18:34:03 +0200

    navigvering

--- js/EventyrView.js
+++ js/EventyrView.js
@@ -5,4 +5,10 @@ var EventyrView = function() {
     return this;
   };
 
-}
+    this.initialize = function() {
+      // Define a div wrapper for the view. The div wrapper is used to attach events.
+      this.el = $('<div/>');
+    }
+
+};
+EventyrView.template = Handlebars.compile($("#eventyr-tpl").html());
