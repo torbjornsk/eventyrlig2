3d37a82acda11edc9d2894aea2ac8b4031af637e
Torbjorn Skyberg Knutsen <torbjornk@mesan.no>
2015-05-10 18:52:59 +0200

    lydavspilling

--- js/EventyrView.js
+++ js/EventyrView.js
@@ -5,10 +5,12 @@ var EventyrView = function() {
     return this;
   };
 
-    this.initialize = function() {
-      // Define a div wrapper for the view. The div wrapper is used to attach events.
-      this.el = $('<div/>');
-    }
+  this.initialize = function() {
+    // Define a div wrapper for the view. The div wrapper is used to attach events.
+    this.el = $('<div/>');
+  };
+
+  this.initialize();
 
 };
 EventyrView.template = Handlebars.compile($("#eventyr-tpl").html());
